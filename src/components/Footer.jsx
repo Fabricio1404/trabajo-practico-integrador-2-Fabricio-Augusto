c4e5bcbe99a3e20721b911907f87b276cc8ab370
Fabricio Augusto <augustofabry@gmail.com>
2025-11-14 01:09:11 -0300

    Crea componente Footer con información básica

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  const year = new Date().getFullYear()
+
+  return (
+    <footer className="w-full bg-gray-900 text-white text-center py-4 mt-10">
+      <p>© {year} </p>
+    </footer>
+  )
+}
