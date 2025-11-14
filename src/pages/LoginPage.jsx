bd791f502597de1e8198af51bd807e0bb7ac8e30
Fabricio Augusto <augustofabry@gmail.com>
2025-11-14 00:21:21 -0300

    Implementa la lógica de envío del formulario de inicio de sesión

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,15 +1,43 @@
+import { useEffect } from 'react';
 import { useForm } from '../hooks/useForm';
-import { useEffect } from 'react';  
+import { useNavigate } from 'react-router';
 
 const LoginPage = () => {
-    const { handleChange, handleReset,formValue  } = useForm({
+    const { handleChange, formValue } = useForm({
         username: '',
         password: ''
-    })
+    });
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      console.log(formValue);
+    }, [formValue]);
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+
+      console.log("estoy en submit");
+      try {
+        const res = await fetch('http://localhost:3000/api/login', {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formValue)
+        });
+        
+        if (res.ok) {
+          navigate("/home");
+        }else{
+          alert("erro es:", error.message);
+        }
+
+      } catch (error) {
+      }
+    };
 
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>Username</label>
             <input 
             name="username" 
